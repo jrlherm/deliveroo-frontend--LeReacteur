6c1f09563b6fda480ebb9f2ed5fd0a8980a65779
JR Lherm <lherm.jr@gmail.com>
2023-08-21 15:38:43 +0200

    Connecting to the backend

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ import Header from "./assets/components/Header";
 // import Restaurant from "./assets/components/Restaurant";
 // import Menu from "./assets/components/Menu";
 
-// import dishImage from "./assets/images/dish-image.jpg";
-// import restaurantImage from "./assets/images/header-image.jpg";
+import dishImage from "./assets/images/dish-image.jpg";
+import restaurantImage from "./assets/images/header-image.jpg";
 
 function App() {
   // State qui contiendra le data (la réponse du serveur)
@@ -19,64 +19,59 @@ function App() {
   useEffect(() => {
     // La fonction du useEffect ne peut pas être asynchrone, je déclare donc une fonction asynchrone à l'intérieur et je l'appelle immédiatement
     const fetchData = async () => {
-      const response = axios.get(
+      const response = await axios.get(
         // Je fais une requête axios et j'attend que le résultat arrive
         "https://site--deliveroo-backend--vm2w9vyj7r62.code.run/"
       );
+      console.log(response.data.header.title);
       setData(response.data); // Je stocke le résultat dans mon state data
       setIsLoading(false); // Je fais paser isLoading à false
-      console.log(response);
     };
     // J'apelle immédiatement ma fonction fetchData
     fetchData();
   }, []);
+
   return (
     <>
-      <Header />
-      {console.log(data)}
-
       {/* Si isLoading === true, c'est que la réponse du serveur n'est pas encore arrivée, donc j'affiche Chargement... afin d'éviter d'avoir une erreur car data est undefined. */}
-      {isLoading ? "Chargement ..." : data}
+      {isLoading ? <p>Chargement ...</p> : <p>{data.header.title}</p>}
+      {console.log(data.header.title)}
+      <div className="main">
+        <Header />
+
+        {/* <Restaurant /> */}
+        <div className="container">
+          <section className="restaurant">
+            <div className="left">
+              <h1 className="restaurant-name">azeaze</h1>
+              <p className="restaurant-description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Consequatur, perferendis dignissimos. Nobis recusandae
+                exercitationem. Lorem ipsum dolor sit amet consectetur
+                adipisicing elit.
+              </p>
+            </div>
+            <div className="right">
+              <img src={restaurantImage} alt="Photo of the restaurant" />
+            </div>
+          </section>
+        </div>
+        {/* <Menu /> */}
+        <div className="menu">
+          <div className="container">
+            <h2>Brunchs</h2>
+            <div className="card">
+              <div className="dish">
+                <div className="dish-desc">Brunch authentique 1 personne</div>
+                <div className="price">00,00€</div>
+              </div>
+              <img src={dishImage} alt="Dish image" className="dish-photo" />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
-  // return (
-
-  // <div className="main">
-  //   <Header />
-
-  //   {/* <Restaurant /> */}
-  //   <div className="container">
-  //     <section className="restaurant">
-  //       <div className="left">
-  //         <h1 className="restaurant-name">Restaurant Name</h1>
-  //         <p className="restaurant-description">
-  //           Lorem ipsum dolor sit amet consectetur adipisicing elit.
-  //           Consequatur, perferendis dignissimos. Nobis recusandae
-  //           exercitationem. Lorem ipsum dolor sit amet consectetur adipisicing
-  //           elit.
-  //         </p>
-  //       </div>
-  //       <div className="right">
-  //         <img src={restaurantImage} alt="Photo of the restaurant" />
-  //       </div>
-  //     </section>
-  //   </div>
-  //   {/* <Menu /> */}
-  //   <div className="menu">
-  //     <div className="container">
-  //       <h2>Brunchs</h2>
-  //       <div className="card">
-  //         <div className="dish">
-  //           <div className="dish-desc">Brunch authentique 1 personne</div>
-  //           <div className="price">00,00€</div>
-  //         </div>
-  //         <img src={dishImage} alt="Dish image" className="dish-photo" />
-  //       </div>
-  //     </div>
-  //   </div>
-  // </div>
-
-  // );
 }
 
 export default App;
